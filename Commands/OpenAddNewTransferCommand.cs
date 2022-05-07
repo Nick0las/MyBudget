67ac6f69e6740a07dc0db25a9252d72b993d00ed
Николай Попов <school1.popov@yandex.ru>
2022-05-07 13:17:13 +0700

    Новое модальное окно для добавления денежных поступлений в БД. Команда открытия окна, ее привязка к меню и кнопке в ToolBar

---
+++ Commands/OpenAddNewTransferCommand.cs
@@ -0,0 +1,30 @@
+﻿using MyBudget.Commands.Base;
+using MyBudget.Views.Windows;
+using System;
+using System.Windows;
+
+namespace MyBudget.Commands
+{
+    class OpenAddNewTransferCommand : Command
+    {
+        private Window_AddNewTransfer _Window;
+        public override bool CanExecute(object parameter) => _Window == null;
+
+        public override void Execute(object parameter)
+        {
+            Window_AddNewTransfer window = new Window_AddNewTransfer
+            {
+                Owner = Application.Current.MainWindow
+            };
+            _Window = window;
+            window.Closed += OnWindowClosed;
+            window.ShowDialog();
+        }
+
+        private void OnWindowClosed(object Sender, EventArgs E)
+        {
+            ((Window)Sender).Closed -= OnWindowClosed;
+            _Window = null;
+        }
+    }
+}
