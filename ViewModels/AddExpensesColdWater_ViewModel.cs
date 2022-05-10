f170bcff0335625e26d6f6a448a4f65d292963e6
Николай Попов <school1.popov@yandex.ru>
2022-05-10 13:06:52 +0700

    Добавлено окно расхода холодной воды

---
+++ ViewModels/AddExpensesColdWater_ViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Data.Sqlite;
+using MyBudget.Commands;
+using MyBudget.Data;
+using MyBudget.Models;
+using MyBudget.Services;
+using MyBudget.Services.Interfaces;
+using MyBudget.ViewModels.Base;
+using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace MyBudget.ViewModels
+{
+    class AddExpensesColdWater_ViewModel : ViewModel_Base
+    {
+        #region Заголовок окна
+        private string _Title = "Добавлние нового расхода холодной воды.";
+        /// <summary>Заголовок окна </summary>
+
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+        #endregion
+
+
+    }
+}
